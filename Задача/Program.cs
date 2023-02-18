23d1a3812d6949ba713b93a9a20726812362de88
Olga Varfolomeeva <romanova_85@mail.ru>
2023-02-18 17:19:03 +0300

    добавлено решение задачи

---
+++ Задача/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] arraybase = new string[5] {"march", "7", "birthday", "38", "lol"};
+string[] array2 = new string[arraybase.Length];
+void ResultMassiv(string[] arraybase, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < arraybase.Length; i++)
+    {
+    if(arraybase[i].Length <= 3)
+        {
+        array2[count] = arraybase[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ResultMassiv(arraybase, array2);
+PrintArray(array2);
+
